afd9bdb9f3c60b8fdb9ffc9bf64657dd9f634aa7
mfaydin49 <mfaydin49@gmail.com>
2022-07-28 15:11:51 +0200

    fixed deploy variable

--- client/src/redux/todos/services.js
+++ client/src/redux/todos/services.js
@@ -1,11 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import env from "react-dotenv";
 
 export const getTodoAsync = createAsyncThunk(
   "todos/getTodosAsync",
   async () => {
-    const res = await axios(`${env.REACT_APP_API_BASE_ENDPOINT}/todos`);
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`);
     return res.data;
   }
 );
@@ -14,7 +13,7 @@ export const addTodoAsync = createAsyncThunk(
   "todos/addTodosAsync",
   async (data) => {
     const res = await axios.post(
-      `${env.REACT_APP_API_BASE_ENDPOINT}/todos`,
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`,
       data
     );
     return res.data;
@@ -25,7 +24,7 @@ export const toggleTodoAsync = createAsyncThunk(
   "todos/toggleTodosAsync",
   async ({ id, data }) => {
     const res = await axios.patch(
-      `${env.REACT_APP_API_BASE_ENDPOINT}/todos/${id}`,
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/todos/${id}`,
       data
     );
     return res.data;
@@ -35,7 +34,9 @@ export const toggleTodoAsync = createAsyncThunk(
 export const removeTodoAsync = createAsyncThunk(
   "todos/removeTodoAsync",
   async (id) => {
-    await axios.delete(`${env.REACT_APP_API_BASE_ENDPOINT}/todos/${id}`);
+    await axios.delete(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/todos/${id}`
+    );
 
     return id;
   }
