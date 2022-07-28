8a737bb4e15ea6367632bf5401130ec6d5a52141
mfaydin49 <mfaydin49@gmail.com>
2022-07-28 15:22:57 +0200

    fixed deploy variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 app.use(cors());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://todo-aydin.netlify.app", credentials: true }));
 
 app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/todos", require("./routers/api/todos"));
